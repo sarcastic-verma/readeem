74ae3219692aa955ed3ebc8b1e6cf73e1ebad407
sarcastic-verma <shivamthegreat.sv@gmail.com>
2020-11-26 21:24:11 +0530

    Initial commit

---
+++ lib/components/google_button.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class GoogleButton extends StatefulWidget {
+  @override
+  _GoogleButtonState createState() => _GoogleButtonState();
+}
+
+class _GoogleButtonState extends State<GoogleButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
