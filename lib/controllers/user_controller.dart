47581fa26f450920e4559a4516a77a5b4c95fc59
sarcastic-verma <shivamthegreat.sv@gmail.com>
2021-01-20 00:43:17 +0530

    - added edit user controller

--- lib/controllers/user_controller.dart
+++ lib/controllers/user_controller.dart
@@ -1,44 +1,39 @@
-
 import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart' as Get;
 import 'package:readeem/getX_controllers/tokens_getX_controller.dart';
 import 'package:readeem/model/user.dart';
 import 'package:readeem/utilities/log_help.dart';
+import 'package:readeem/utilities/routes.dart';
 import 'package:readeem/utilities/unauthorized_wrapper.dart';
 
 class UserController {
-  static Future<User> editUserController() async {
+  static Future<User> editUserController(
+      {@required String name,
+      @required String email,
+      @required String password,
+      @required String mobile,
+      @required String imgHash}) async {
     try {
       Dio _dio = Dio(
         BaseOptions(
           headers: {
             'Authorization':
                 'Bearer ${Get.Get.find<TokensGetXController>().accessToken}',
-            // 'X-Requested-With': 'XMLHttpRequest',
           },
           contentType: Headers.formUrlEncodedContentType,
         ),
       )..interceptors.addAll([unauthorizedWrapper()]);
+      final response = await _dio
+          .patch(editUser, data: {name, email, password, mobile, imgHash});
+      if (response.data['status'] == 'success') {
+        return User.fromJson(response.data['user']);
+      } else {
+        return null;
+      }
     } catch (err) {
       logger.e(err);
       return null;
     }
-
-    return User(
-      name: null,
-      previousOrders: null,
-      changePasswordDate: null,
-      reward: null,
-      id: null,
-      joinDate: null,
-      mobile: null,
-      img: null,
-      email: null,
-      cart: null,
-      favBooks: null,
-      completedBooks: null,
-      inProgressBooks: null,
-    );
   }
-
 }
